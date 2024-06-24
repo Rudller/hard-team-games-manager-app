41dfb6f271fedf9e2ebe328981e5d436f207921f
Rudller <junior.rutkowski@gmail.com>
2024-06-24 22:13:44 +0200

    Update Header.jsx

--- hard-team-game-manage-app/src/components/Header.jsx
+++ hard-team-game-manage-app/src/components/Header.jsx
@@ -1,6 +1,10 @@
 export default function Header() {
     const user = JSON.parse(localStorage.getItem("user"))
 
+    const logoutHandler = () => {
+        localStorage.removeItem("user")
+        window.location.reload()
+    }
 
     return (
         <header className="flex justify-between items-center bg-orange-500 text-white border-solid border border-transparent p-4 h-16">
@@ -10,8 +14,8 @@ export default function Header() {
                 <a href="/singup" className="inline-block mx-2 px-3 border-x-2 border-orange-700">Zarejestruj</a>
                 </nav>}
             {user && <div>
-                <p className="inline-block mx-2 px-3 border-x-2 border-orange-700">Zalogowano jako: {user.username}</p>
-                <a href="/" className="inline-block mx-2 px-3 border-x-2 border-orange-700">Wyloguj</a>
+                <div className="inline-block mx-2 px-3 border-x-2 border-orange-700">Zalogowano jako: {user.username}</div>
+                <button onClick={logoutHandler} href="/" className="inline-block mx-2 px-3 border-x-2 border-orange-700">Wyloguj</button>
             </div>}
         </header>
     )
